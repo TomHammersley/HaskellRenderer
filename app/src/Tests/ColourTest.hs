98206bedb996b1572fc492ff85d53952cd9f6bcd
Tom Hammersley <tomhammersley@gmail.com>
2011-09-29 21:10:09 +0100

    Fix unit tests

--- app/src/Tests/ColourTest.hs
+++ app/src/Tests/ColourTest.hs
@@ -1,42 +1,28 @@
 module Tests.ColourTest where
 
+import PolymorphicNum
 import Colour
 import Test.HUnit
 
-test_Add = TestCase (assertEqual "Colour addition" expected (v1 + v2))
+test_Add = TestCase (assertEqual "Colour addition" expected (v1 <+> v2))
     where
       v1 = Colour 1 2 3 4
       v2 = Colour 10 20 30 40
       expected = Colour 11 22 33 44
 
-test_Sub = TestCase (assertEqual "Colour subtraction" expected (v1 - v2))
+test_Sub = TestCase (assertEqual "Colour subtraction" expected (v1 <-> v2))
     where
       v1 = Colour 10 20 30 40
       v2 = Colour 5 10 15 20
       expected = Colour 5 10 15 20
 
-test_Mul = TestCase (assertEqual "Colour multiplication" expected (v1 * v2))
+test_Mul = TestCase (assertEqual "Colour multiplication" expected (v1 <*> v2))
     where
       v1 = Colour 10 20 30 40
       v2 = Colour 5 10 15 20
       expected = Colour 50 200 450 800
 
-test_Abs = TestCase (assertEqual "Colour abs" expected (abs v1))
-    where
-      v1 = Colour 10 (-20) 0 (-40)
-      expected = Colour 10 20 0 40
-
-test_Signum = TestCase (assertEqual "Colour signum" expected (signum v1))
-    where
-      v1 = Colour 10 (-20) 0 (-40)
-      expected = Colour 1 (-1) 0 (-1)
-
-test_fromInteger = TestCase (assertEqual "Colour fromInteger" expected (signum v1))
-    where
-      v1 = fromInteger 5
-      expected = Colour 5 5 5 5
-
-test_Div = TestCase (assertEqual "Colour division" expected (v1 / v2))
+test_Div = TestCase (assertEqual "Colour division" expected (v1 </> v2))
     where
       v1 = Colour 10 20 30 40
       v2 = Colour 5 10 15 20
@@ -45,13 +31,13 @@ test_Div = TestCase (assertEqual "Colour division" expected (v1 / v2))
 test_AddScalar = TestCase (assertEqual "Colour add scalar" expected (v1 <+> k))
     where
       v1 = Colour 10 20 30 40
-      k = 1
+      k = 1 :: Double
       expected = Colour 11 21 31 41
 
 test_SubScalar = TestCase (assertEqual "Colour sub scalar" expected (v1 <-> k))
     where
       v1 = Colour 10 20 30 40
-      k = 1
+      k = 1 :: Double
       expected = Colour 9 19 29 39
 
 test_Clamp = TestCase (assertEqual "Colour clamp" expected (clamp v1))
@@ -73,8 +59,6 @@ tests_Colour = TestList [
                 TestLabel "Addition" test_Add,
                 TestLabel "Subtraction" test_Sub,
                 TestLabel "Multiplication" test_Mul,
-                TestLabel "Abs" test_Abs,
-                TestLabel "Signum" test_Signum,
                 TestLabel "Div" test_Div,
                 TestLabel "Add scalar" test_AddScalar,
                 TestLabel "Sub scalar" test_SubScalar,
--- app/src/Tests/VectorTest.hs
+++ app/src/Tests/VectorTest.hs
@@ -1,40 +1,26 @@
 module Tests.VectorTest where
 
+import PolymorphicNum
 import Vector
 import Test.HUnit
 
-test_Add = TestCase (assertEqual "Vector addition" expectedResult (v1 + v2))
+test_Add = TestCase (assertEqual "Vector addition" expectedResult (v1 <+> v2))
     where
       v1 = Vector 1 2 3 4
       v2 = Vector 10 20 30 40
       expectedResult = Vector 11 22 33 44
 
-test_Sub = TestCase (assertEqual "Vector subtraction" expectedResult (v1 - v2))
+test_Sub = TestCase (assertEqual "Vector subtraction" expectedResult (v1 <-> v2))
     where
       v1 = Vector 10 20 30 40
       v2 = Vector 1 2 3 4
       expectedResult = Vector 9 18 27 36
 
-test_Mul = TestCase (assertEqual "Vector multiplication" expectedResult (v1 * v2))
+test_Mul = TestCase (assertEqual "Vector multiplication" expectedResult (v1 <*> v2))
     where
       v1 = Vector 1 0 2 3
       v2 = Vector 1 10 (-2) 3
       expectedResult = Vector 1 0 (-4) 9
-
-test_Negate = TestCase (assertEqual "Vector negation" expectedResult (Vector.negate v1))
-    where
-      v1 = Vector 1 (-2) 3 (-4)
-      expectedResult = Vector (-1) 2 (-3) 4
-
-test_Abs = TestCase (assertEqual "Vector abs" expectedResult (abs v1))
-    where
-      v1 = Vector 0 1 (-2) 3
-      expectedResult = Vector 0 1 2 3
-
-test_Signum = TestCase (assertEqual "Vector signum" expectedResult (signum v1))
-    where
-      v1 = Vector 1 (-1) 0 (-2)
-      expectedResult = Vector 1 (-1) 0 (-1)
 
 test_Madd = TestCase (assertEqual "Vector madd" expectedResult (madd pos dir k))
     where
@@ -43,16 +29,16 @@ test_Madd = TestCase (assertEqual "Vector madd" expectedResult (madd pos dir k))
       k = 10
       expectedResult = Vector 6 2 13 1
 
-test_ScalarMul = TestCase (assertEqual "Vector-scalar mul" expectedResult (vectorScalarMul vec k))
+test_ScalarMul = TestCase (assertEqual "Vector-scalar mul" expectedResult (vec <*> k))
     where
       vec = Vector 1 2 (-3) 1
-      k = 2
+      k = 2 :: Double
       expectedResult = Vector 2 4 (-6) 2
 
 test_ScalarDiv = TestCase (assertEqual "Vector-scalar div" expectedResult (vec </> k))
     where
       vec = Vector 10 20 (-30) 40
-      k = 2
+      k = 2 :: Double
       expectedResult = Vector 5 10 (-15) 20
 
 test_ScalarDot3 = TestCase (assertEqual "dot3" expectedResult (v1 `dot3` v2))
@@ -124,9 +110,6 @@ tests_Vector = TestList [
                 TestLabel "Addition" test_Add, 
                 TestLabel "Subtraction" test_Sub,
                 TestLabel "Multiplication" test_Mul,
-                TestLabel "Negation" test_Negate,
-                TestLabel "Abs" test_Abs,
-                TestLabel "Signum" test_Signum,
                 TestLabel "Madd" test_Madd,
                 TestLabel "Vector * scalar" test_ScalarMul,
                 TestLabel "Vector / scalar" test_ScalarDiv,
